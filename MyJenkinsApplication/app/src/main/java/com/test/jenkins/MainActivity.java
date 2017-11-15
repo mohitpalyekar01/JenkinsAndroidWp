d260e6c179c6af475a435a40802fd591580136ec
Mohit Palyekar <mohit.palyekar@creativecapsule.com>
2017-11-15 12:24:48 +0530

    Flavour based url

--- MyJenkinsApplication/app/src/main/java/com/test/jenkins/MainActivity.java
+++ MyJenkinsApplication/app/src/main/java/com/test/jenkins/MainActivity.java
@@ -2,14 +2,18 @@ package com.test.jenkins;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textView=(TextView)findViewById(R.id.url_tv);
+        textView.setText(BuildConfig.URL);
         //test auto build tests 123
     }
 }
