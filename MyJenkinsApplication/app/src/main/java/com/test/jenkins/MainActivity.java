3ff70a4f157f91ca6eca4d804d8dff4753b2084a
Mohit Palyekar <mohit.palyekar@creativecapsule.com>
2017-12-19 17:03:27 +0530

    feature added

--- MyJenkinsApplication/app/src/main/java/com/test/jenkins/MainActivity.java
+++ MyJenkinsApplication/app/src/main/java/com/test/jenkins/MainActivity.java
@@ -7,6 +7,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
+
+    //test feature added
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
